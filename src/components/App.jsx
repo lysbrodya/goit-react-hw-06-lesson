3233d8c8155028d887888cf41025dc3867385e77
lysbrodya <lysbrodya@gmail.com>
2025-07-18 18:19:10 +0300

    l7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,26 @@
-import { Layout } from './Layout/Layout';
-import { AppBar } from './AppBar/AppBar';
-import { TaskForm } from './TaskForm/TaskForm';
-import { TaskList } from './TaskList/TaskList';
+import { Layout } from "./Layout/Layout";
+import { AppBar } from "./AppBar/AppBar";
+import { TaskForm } from "./TaskForm/TaskForm";
+import { TaskList } from "./TaskList/TaskList";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTasks } from "../redux/operations";
 
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state.tasks.isLoading);
+  const error = useSelector((state) => state.tasks.error);
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+    return () => {};
+  }, [dispatch]);
+
   return (
     <Layout>
       <AppBar />
       <TaskForm />
+      {isLoading && !error && <b>In progress ...</b>}
       <TaskList />
     </Layout>
   );
--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -50,53 +50,111 @@
 //   }
 // }
 
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
-const slice = createSlice({
+// const slice = createSlice({
+//   name: "tasks",
+//   initialState: {
+//     items: [
+//       { id: 0, text: "Learn HTML and CSS", completed: true },
+//       { id: 1, text: "Get good at JavaScript", completed: true },
+//       { id: 2, text: "Master React", completed: false },
+//       { id: 3, text: "Discover Redux", completed: false },
+//       { id: 4, text: "Build amazing apps", completed: false },
+//     ],
+//   },
+//   reducers: {
+//     addTask(state, action) {
+//       state.items.push(action.payload);
+//     },
+//     deleteTask: (state, action) => {
+//       //   return {
+//       //     ...state.items = state.items.filter((task) => task.id !== action.payload),
+//       //   };
+//       state.items = state.items.filter((item) => item.id !== action.payload);
+//     },
+//     toggleCompleted: (state, action) => {
+//       //   return {
+//       //     ...state,
+//       //     items: state.items.map((task) => {
+//       //       if (task.id !== action.payload) {
+//       //         return task;
+//       //       }
+//       //       return {
+//       //         ...task,
+//       //         completed: !task.completed,
+//       //       };
+//       //     }),
+//       //   };
+//       for (const task of state.items) {
+//         if (task.id === action.payload) {
+//           task.completed = !task.completed;
+//           break;
+//         }
+//       }
+//     },
+//   },
+// });
+
+// export const { addTask, deleteTask, toggleCompleted } = slice.actions;
+
+// // Експортуємо редюсер слайсу
+// export default slice.reducer;
+
+// 7я лекция всё заново нахуй
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchTasks, addTask, deleteTask, toggleCompleted } from "./operations";
+const handlePending = (state) => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
+const tasksSlice = createSlice({
   name: "tasks",
   initialState: {
-    items: [
-      { id: 0, text: "Learn HTML and CSS", completed: true },
-      { id: 1, text: "Get good at JavaScript", completed: true },
-      { id: 2, text: "Master React", completed: false },
-      { id: 3, text: "Discover Redux", completed: false },
-      { id: 4, text: "Build amazing apps", completed: false },
-    ],
+    items: [],
+    isLoading: false,
+    error: null,
   },
-  reducers: {
-    addTask(state, action) {
-      state.items.push(action.payload);
-    },
-    deleteTask: (state, action) => {
-      //   return {
-      //     ...state.items = state.items.filter((task) => task.id !== action.payload),
-      //   };
-      state.items = state.items.filter((item) => item.id !== action.payload);
-    },
-    toggleCompleted: (state, action) => {
-      //   return {
-      //     ...state,
-      //     items: state.items.map((task) => {
-      //       if (task.id !== action.payload) {
-      //         return task;
-      //       }
-      //       return {
-      //         ...task,
-      //         completed: !task.completed,
-      //       };
-      //     }),
-      //   };
-      for (const task of state.items) {
-        if (task.id === action.payload) {
-          task.completed = !task.completed;
-          break;
-        }
-      }
-    },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTasks.pending, handlePending)
+      .addCase(fetchTasks.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items = action.payload;
+      })
+      .addCase(fetchTasks.rejected, handleRejected)
+      .addCase(addTask.pending, handlePending)
+      .addCase(addTask.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items.push(action.payload);
+      })
+      .addCase(addTask.rejected, handleRejected)
+      .addCase(deleteTask.pending, handlePending)
+      .addCase(deleteTask.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items = state.items.filter(
+          (task) => task.id !== action.payload.id
+        );
+      })
+      .addCase(deleteTask.rejected, handleRejected)
+      .addCase(toggleCompleted.pending, handlePending)
+      .addCase(toggleCompleted.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items = state.items.map((task) =>
+          task.id === action.payload.id ? action.payload : task
+        );
+      })
+      .addCase(toggleCompleted.rejected, handleRejected);
   },
 });
 
-export const { addTask, deleteTask, toggleCompleted } = slice.actions;
-
-// Експортуємо редюсер слайсу
-export default slice.reducer;
+export default tasksSlice.reducer;
